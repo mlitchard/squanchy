6b45c4f5d96c543f0895e3a6c4a5dddb6dd7379f
mlitchard <mlitchard@hermes.localdomain>
2017-08-29 21:24:04 -0700

    evaluation seems to be working

--- src/Eval.hs
+++ src/Eval.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Eval where
 
 import BasicPrelude
@@ -7,15 +8,16 @@ import Types
 eval :: Expr a -> a
 eval (BoolConst a)     = a
 eval (NumberConst a)   = a
+eval (SquanchyString a) = a
+eval (SquanchyVar name val) = (,) name (eval val)
+eval (Not a)           = not (eval a)
+eval (And a b)         = (eval a) && (eval b)
+eval (Or a b)          = (eval a) || (eval b)
+eval (Xor a b)         = (eval a) || (eval b) && (not (eval a && eval b))
 
-eval (Not a)           = not a
-eval (And a b)         = a && b
-eval (Or a b)          = a || b
-eval (Xor a b)         = a || b && (not (a && b))
-
-eval (Equals a b)      = (eval a) `equals` (eval b)
-eval (GreaterThan a b) = (eval a) `greaterThan` (eval b)
-eval (LessThan a b)    = (eval a) `lessThan` (eval b)
+eval (Equals a b)      = (eval a) == (eval b)
+eval (GreaterThan a b) = (eval a) > (eval b)
+eval (LessThan a b)    = (eval a) < (eval b)
 
 eval (Div a b)         = (eval a) `divide` (eval b)
 eval (Mul a b)         = (eval a) `mul`    (eval b)
--- src/Types.hs
+++ src/Types.hs
@@ -1,62 +1,67 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+
 module Types where
 
 import BasicPrelude
 
 data Expr a where
-  BoolConst   :: Bool -> Expr Bool
-  NumberConst :: Number a -> (Expr (Number a))
-
+  BoolConst   ::  Bool -> Expr Bool
+  NumberConst :: (Num a) => a -> Expr a
+  SquanchyString :: Text -> Expr Text
+  SquanchyVar :: Text -> Expr a -> Expr (Text, a) 
   Not         :: Expr Bool -> Expr Bool
   And         :: Expr Bool -> Expr Bool -> Expr Bool
   Or          :: Expr Bool -> Expr Bool -> Expr Bool
   Xor         :: Expr Bool -> Expr Bool -> Expr Bool
-
-  Equals      :: (SquanchyNum n) => Expr (Number n)
-                                 -> Expr (Number n)
-                                 -> Expr Bool
-  GreaterThan :: (SquanchyNum n) => Expr (Number n)
-                                 -> Expr (Number n)
-                                 -> Expr Bool
-  LessThan    :: (SquanchyNum n) => Expr (Number n)
-                                 -> Expr (Number n)
-                                 -> Expr Bool
+  
+  Equals      :: (Eq n) => Expr n -> Expr n -> Expr Bool
+                 
+  GreaterThan :: (Ord n) => Expr n
+                         -> Expr n
+                         -> Expr Bool
+                 
+  LessThan    :: (Ord n) => Expr n
+                         -> Expr n
+                         -> Expr Bool
 
   Div         :: (SquanchyNum n) => Expr (Number n)
                                  -> Expr (Number n)
                                  -> Expr n
+                 
   Mul         :: (SquanchyNum n) => Expr (Number n)
                                  -> Expr (Number n)
                                  -> Expr n
+                 
   Sub         :: (SquanchyNum n) => Expr (Number n)
                                  -> Expr (Number n)
                                  -> Expr n
+                 
   Add         :: (SquanchyNum n) => Expr (Number n)
                                  -> Expr (Number n)
                                  -> Expr n
+data family Number :: * -> *
+data instance Number Integer = NumInt Integer deriving (Eq, Ord, Show)
+data instance Number Float   = NumFloat Float deriving (Eq, Ord, Show)
 
-class SquanchyNum a where
-  data Number a :: *
-
-
-  equals :: Number a -> Number a -> Bool     
-  greaterThan :: Number a -> Number a -> Bool
-  lessThan    :: Number a -> Number a -> Bool
+class (Eq a, Ord a, Num a) => SquanchyNum a where
 
   divide :: Number a -> Number a -> a
   mul    :: Number a -> Number a -> a
   sub    :: Number a -> Number a -> a
   add    :: Number a -> Number a -> a
-
+  
 instance SquanchyNum Integer where
-  data Number Integer = NumInt Integer deriving (Eq,Show)
-
-  equals a b = a == b
-  greaterThan (NumInt a) (NumInt b) = a > b
-  lessThan (NumInt a) (NumInt b)    = a < b
 
   divide (NumInt x) (NumInt y) = x `div` y
   mul    (NumInt x) (NumInt y) = x * y
   sub    (NumInt x) (NumInt y) = x - y
   add    (NumInt x) (NumInt y) = x + y
+  
+instance SquanchyNum Float where
+
+  divide (NumFloat x) (NumFloat y) = x / y
+  mul    (NumFloat x) (NumFloat y) = x * y
+  sub    (NumFloat x) (NumFloat y) = x - y
+  add    (NumFloat x) (NumFloat y) = x + y
