c897bd52d1ed162886b20d2365c3ab78dca0b8ed
Michael Litchard <litchard.michael@gmail.com>
2022-01-13 17:11:43 +0000

    fixed fundamental logic with Expr a

--- src/Eval.hs
+++ src/Eval.hs
@@ -9,7 +9,6 @@ eval :: Expr a -> a
 eval (BoolConst a)     = a
 eval (NumberConst a)   = a
 eval (SquanchyString a) = a
-eval (SquanchyVar name val) = (,) name (eval val)
 eval (Not a)           = not (eval a)
 eval (And a b)         = (eval a) && (eval b)
 eval (Or a b)          = (eval a) || (eval b)
@@ -20,6 +19,6 @@ eval (GreaterThan a b) = (eval a) > (eval b)
 eval (LessThan a b)    = (eval a) < (eval b)
 
 eval (Div a b)         = (eval a) `divide` (eval b)
-eval (Mul a b)         = (eval a) `mul`    (eval b)
-eval (Sub a b)         = (eval a) `sub`    (eval b)
-eval (Add a b)         = (eval a) `add`    (eval b)
+eval (Mul a b)         = (eval a) * (eval b)
+eval (Sub a b)         = (eval a) - (eval b)
+eval (Add a b)         = (eval a) + (eval b)
--- src/Types.hs
+++ src/Types.hs
@@ -6,11 +6,16 @@ module Types where
 
 import BasicPrelude
 
+class (Num a, Eq a, Ord a) => Divisible a where
+  divide :: a -> a -> a
+
+instance Divisible Int where divide = div
+instance Divisible Float where divide = (/)
+
 data Expr a where
   BoolConst   ::  Bool -> Expr Bool
-  NumberConst :: (Num a) => a -> Expr a
+  NumberConst :: (Divisible a) => a -> Expr a
   SquanchyString :: Text -> Expr Text
-  SquanchyVar :: Text -> Expr a -> Expr (Text, a) 
   Not         :: Expr Bool -> Expr Bool
   And         :: Expr Bool -> Expr Bool -> Expr Bool
   Or          :: Expr Bool -> Expr Bool -> Expr Bool
@@ -18,50 +23,14 @@ data Expr a where
   
   Equals      :: (Eq n) => Expr n -> Expr n -> Expr Bool
                  
-  GreaterThan :: (Ord n) => Expr n
-                         -> Expr n
-                         -> Expr Bool
+  GreaterThan :: (Ord n) => Expr n -> Expr n -> Expr Bool
                  
-  LessThan    :: (Ord n) => Expr n
-                         -> Expr n
-                         -> Expr Bool
+  LessThan    :: (Ord n) => Expr n -> Expr n -> Expr Bool
 
-  Div         :: (SquanchyNum n) => Expr (Number n)
-                                 -> Expr (Number n)
-                                 -> Expr n
+  Div         :: (Divisible n) => Expr n -> Expr n -> Expr n
                  
-  Mul         :: (SquanchyNum n) => Expr (Number n)
-                                 -> Expr (Number n)
-                                 -> Expr n
+  Mul         :: (Divisible n) => Expr n -> Expr n -> Expr n
                  
-  Sub         :: (SquanchyNum n) => Expr (Number n)
-                                 -> Expr (Number n)
-                                 -> Expr n
+  Sub         :: (Divisible n) => Expr n -> Expr n -> Expr n
                  
-  Add         :: (SquanchyNum n) => Expr (Number n)
-                                 -> Expr (Number n)
-                                 -> Expr n
-data family Number :: * -> *
-data instance Number Integer = NumInt Integer deriving (Eq, Ord, Show)
-data instance Number Float   = NumFloat Float deriving (Eq, Ord, Show)
-
-class (Eq a, Ord a, Num a) => SquanchyNum a where
-
-  divide :: Number a -> Number a -> a
-  mul    :: Number a -> Number a -> a
-  sub    :: Number a -> Number a -> a
-  add    :: Number a -> Number a -> a
-  
-instance SquanchyNum Integer where
-
-  divide (NumInt x) (NumInt y) = x `div` y
-  mul    (NumInt x) (NumInt y) = x * y
-  sub    (NumInt x) (NumInt y) = x - y
-  add    (NumInt x) (NumInt y) = x + y
-  
-instance SquanchyNum Float where
-
-  divide (NumFloat x) (NumFloat y) = x / y
-  mul    (NumFloat x) (NumFloat y) = x * y
-  sub    (NumFloat x) (NumFloat y) = x - y
-  add    (NumFloat x) (NumFloat y) = x + y
+  Add         :: (Divisible n) => Expr n -> Expr n -> Expr n
