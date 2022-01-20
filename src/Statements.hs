614100559d84d30ee5bef58ad216d7bcbc7bb348
Michael Litchard <litchard.michael@gmail.com>
2022-01-20 22:04:36 +0000

    while is broken

--- src/Statements.hs
+++ src/Statements.hs
@@ -18,16 +18,22 @@ data Stmt
  | While Value Stmt
  | Seq [Stmt]
 
-exec :: Stmt -> EvalMonad ()
-exec (x := e)      = modify $ M.insert x e
+exec :: Prog -> EvalMonad ()
+exec (var := val)  = modify $ M.insert var val
 exec (While e s)   = do
-                        let expr :: (Maybe (Expr Bool))
-                            expr = toExpr e 
-                        case expr of
-                          Just (BoolConst res) -> do
-                                                   when (not res)    
-                                                     $ exec (Seq [s, While e s])
-                          _      -> error whileError 
+  let expr :: (Maybe (Expr Bool))
+      expr = toExpr e 
+  case expr of
+    Just res -> do
+                  res' :: Value <- eval res
+                  let expr' :: (Maybe (Expr Bool))
+                      expr' = castBool res'
+                     
+                  case expr' of
+                    Just (BoolConst b) -> when (not b) 
+                                            $ exec (Seq [s, While e s])
+                    p                  -> error $ reductionError p
+    _      -> error whileError 
 exec (Seq [])      = return ()
 exec (Seq (s:ss))  = do
                        exec s
@@ -35,8 +41,45 @@ exec (Seq (s:ss))  = do
 
 whileError :: String
 whileError = "While needs a boolean to evaluate"
-{-
-fib :: Prog Value
+
+reductionError :: Maybe (Expr Bool) -> String
+reductionError Nothing = "Not a bool"
+reductionError (Just (Sub _ _)) = "it never reduced the sub"
+reductionError x'' = 
+  "While couldn't reduce the boolean value for some reason " 
+    ++ (show $ typeOf x'')
+
+zero :: Value
+zero = Value $ NumberConst (0 :: Int)
+
+one :: Value
+one = Value $ NumberConst (1 :: Int)
+
+booleanTest :: Value
+booleanTest = Value $ (Not (Equals (SquanchyVar "n") (NumberConst (1 :: Int))))
+
+x :: Expr Int
+x = SquanchyVar "x"
+
+y :: Expr Int
+y = SquanchyVar "y"
+
+z :: Expr Int
+z = SquanchyVar "z"
+
+n :: Expr Int
+n = SquanchyVar "n"
+
+fib :: Prog
 fib = Seq
-  [ "x" := NumberConst (0 :: Int)]
--}
+  [ "x" := zero
+  , "y" := one
+  , While booleanTest $ Seq 
+      [ "z" := (Value (Add x y)) 
+      , "x" := (Value y)
+      , "y" := (Value z)
+      , "n" := (Value ((Sub n (NumberConst 1 :: Expr Int))))    
+      ]
+       
+ ]
+
