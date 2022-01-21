b8b8dd2e3a6fb3da3b9402807a2848ee88e4b781
Michael Litchard <litchard.michael@gmail.com>
2022-01-21 19:04:21 +0000

    okay yeah, ready

--- src/Statements.hs
+++ src/Statements.hs
@@ -82,38 +82,4 @@ reductionError (Just (Sub _ _)) = "it never reduced the sub"
 reductionError x'' = 
   "While couldn't reduce the boolean value for some reason " 
     ++ (show $ typeOf x'')
-{-
-zero :: Value
-zero = Value $ NumberConst (0 :: Int)
 
-one :: Value
-one = Value $ NumberConst (1 :: Int)
-
-booleanTest :: Value
-booleanTest = Value $ (Not (Equals (NumberConst (1 :: Int)) (SquanchyVar "n")))
-
-x :: Expr Int
-x = SquanchyVar "x"
-
-y :: Expr Int
-y = SquanchyVar "y"
-
-z :: Expr Int
-z = SquanchyVar "z"
-
-n :: Expr Int
-n = SquanchyVar "n"
-
-fib :: Prog
-fib = Seq
-  [ "x" := zero
-  , "y" := one
-  , While booleanTest $ Seq 
-      [ "z" := (Value (Add x y)) 
-      , "x" := (Value y)
-      , "y" := (Value z)
-      , "n" := (Value ((Sub n (NumberConst 1 :: Expr Int) )))    
-      ]
-       
- ]
--}
